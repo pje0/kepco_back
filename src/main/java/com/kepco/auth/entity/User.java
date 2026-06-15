59283dd6bb7a5b3010535da1075e03ad774b78df
홍현민 <aquarius8925@gmail.com>
2026-06-15 17:38:01 +0900

    Update User.java

--- src/main/java/com/kepco/auth/entity/User.java
+++ src/main/java/com/kepco/auth/entity/User.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "users")
+@Table(name = "users", schema = "public")
 @Getter @Setter
 @NoArgsConstructor
 public class User {

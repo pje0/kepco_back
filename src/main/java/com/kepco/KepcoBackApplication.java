435ed10e614b75055b63a4a253bf6afbc914f206
pje0 <pje.jungeun@gmail.com>
2026-06-15 15:50:35 +0900

    Initial commit

---
+++ src/main/java/com/kepco/KepcoBackApplication.java
@@ -0,0 +1,13 @@
+package com.kepco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KepcoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KepcoBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kepco/KepcoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kepco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KepcoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
